112bf8bd0e20fa5fc29b7ee6521e07c81c3bb7a3
Hassan Ahamed <hassanahamed@Hassans-MacBook-Air.local>
2024-12-19 12:41:10 +0530

    Added test cases..

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck
 // which is a slice of strings
@@ -27,5 +34,56 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	// cardsString := ""
+
+	// for i, card;= range d{
+	// 	cardsString += fmt.Sprintf("%v ", i+1) + card + "\n"
+	// }
+
+	// return cardsString
+
+	return strings.Join([]string(d), ",")
+}
+
+// saveToFile writes the deck to a file with the given filename.//+
+// Parameters://+
+//   - filename: A string representing the name of the file to save the deck to.//+
+//
+// Returns://+
+//   - An error if the write operation fails, otherwise nil.//+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+
+	r := rand.New(source)
+
+	for i := range d {
+		j := r.Intn(len(d) - 1)
+		d[i], d[j] = d[j], d[i]
 	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,36 +1,47 @@
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	fmt.Println("Build Cards Game!!")
-// 	// card := newCard()
-// 	// fmt.Println(card)
-
-// 	// cards := deck{"Ace of Diamonds", newCard()}
-
-// 	cards := newDeck()
-
-// 	// cards = append(cards, "Six of Spades")
-
-// 	fmt.Println(cards)
-
-// 	// for i, card := range cards {
-// 	// 	fmt.Println(i, card)
-// 	// }
-
-// 	cards.print()
-// }
-
-// // func newCard() string {
-// // 	return "Five of Diamonds"
-// // }
-
 package main
 
 import "fmt"
 
 func main() {
-	deckSize = 52
-	fmt.Println(deckSize)
+	fmt.Println("Build Cards Game!!")
+	// cards := newCard()
+	// fmt.Println(card)
+
+	// cards := deck{"Ace of Diamonds", newCard()}
+
+	cards := newDeck()
+
+	// cards := newDeckFromFile("myCards")
+
+	cards.shuffle()
+
+	cards.print()
+
+	// fmt.Println(cards.toString())
+
+	// cards.saveToFile("myCards")
+	// cards.newDeckFromFile("myCards")
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// cards = append(cards, "Six of Spades")
+
+	// fmt.Println(cards)
+
+	// fmt.Println(hand)
+	// fmt.Println(remainingCards)
+
+	// // for i, card := range cards {
+	// // 	fmt.Println(i, card)
+	// // }
+
+	// cards.print()
+
 }
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
